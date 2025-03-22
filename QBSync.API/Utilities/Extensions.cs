54a963afb2cf52d0198bd3785253d349e4577632
travis@travestyofcode.com <travis@travestyofcode.com>
2025-03-22 12:14:36 -0400

    Adds string extensions for null and empty checks.

---
+++ QBSync.API/Utilities/Extensions.cs
@@ -0,0 +1,7 @@
+﻿namespace QBSync.API;
+
+public static class Extensions
+{
+    public static bool IsNullOrEmpty(this string? value) => string.IsNullOrWhiteSpace(value);
+    public static bool IsNotNullOrEmpty(this string? value) => !string.IsNullOrWhiteSpace(value);
+}
