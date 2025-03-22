c4a2c302106c5dc309d85f797741843b860a91ba
travis@travestyofcode.com <travis@travestyofcode.com>
2025-03-22 13:10:40 -0400

    Adds Results and Errors for responses to any query or request.

---
+++ QBSync.API/Error.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace QBSync.API;
+
+public abstract class Error
+{
+    public int StatusCode { get; set; }
+    public required string StatusMessage { get; set; }
+    public ModelStateDictionary ModelState { get; } = new ModelStateDictionary();
+}
+
+public class BadRequestError : Error
+{
+    public BadRequestError() : this("Bad Request") { }
+    public BadRequestError(string errorMessage)
+    {
+        StatusCode = 400;
+        StatusMessage = errorMessage;
+    }
+    public BadRequestError(string property, string propertyError) : this("Bad Request")
+    {
+        ModelState.AddModelError(property, propertyError);
+    }
+}
+
+public class ForbiddenError : Error
+{
+    public ForbiddenError() : this("Forbidden") { }
+    public ForbiddenError(string errorMessage)
+    {
+        StatusCode = 403;
+        StatusMessage = errorMessage;
+    }
+}
+
+public class NotFoundError : Error
+{
+    public NotFoundError() : this("Not Found") { }
+    public NotFoundError(string errorMessage)
+    {
+        StatusCode = 404;
+        StatusMessage = errorMessage;
+    }
+    public NotFoundError(string key, string message) : this("Not Found")
+    {
+        ModelState.AddModelError(key, message);
+    }
+}
+
+public class ServerError : Error
+{
+    public ServerError() : this("Server Error") { }
+    public ServerError(string errorMessage)
+    {
+        StatusCode = 500;
+        StatusMessage = errorMessage;
+    }
+}
---
+++ QBSync.API/Result.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace QBSync.API;
+
+public class Result
+{
+    public int StatusCode { get; set; }
+    public required string StatusMessage { get; set; }
+    public bool WasSuccessful => StatusCode >= 200 && StatusCode <= 299;
+    public List<Error> Errors { get; } = new List<Error>();
+
+    public static Result Ok() => new Result() { StatusCode = 200, StatusMessage = "Ok" };
+}
+
+public class Result<T> : Result
+{
+    private T? _Value;
+    public T? Value
+    {
+        get => WasSuccessful ? _Value : throw new InvalidOperationException("Unable to access the Value of a non successful Result.");
+        set
+        {
+            _Value = value;
+        }
+    }
+
+    [SetsRequiredMembers]
+    internal Result(int statusCode, string message, T? value)
+    {
+        StatusCode = statusCode;
+        StatusMessage = message;
+        _Value = value;
+    }
+
+    [SetsRequiredMembers]
+    internal Result(int statusCode, string message, T? value, Error error) : this(statusCode, message, value)
+    {
+        Errors.Add(error);
+    }
+
+    public static Result<T> Ok(T value) => new Result<T>(200, "Ok", value);
+    public static implicit operator Result<T>(T value) => new Result<T>(200, "Ok", value);
+    public static implicit operator Result<T>(Error error) => new Result<T>(error.StatusCode, error.StatusMessage, default, error);
+
+}
