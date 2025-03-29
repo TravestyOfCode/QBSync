79409be4b0ee3b285ffee0ceaf9d42bdb89906ac
travis@travestyofcode.com <travis@travestyofcode.com>
2025-03-29 07:13:36 -0400

    Corrects folder structure and namespace declarations.

---
+++ QBSync.API/QBSDK/SalesOrders/CreateSalesOrder.cs
@@ -0,0 +1,65 @@
+﻿using MediatR;
+using System.Xml.Linq;
+
+namespace QBSync.API.QBSDK;
+
+public class CreateSalesOrder : IRequest<Result<SalesOrder>>
+{
+    public string? TxnID { get; set; }
+    public string? EditSequence { get; set; }
+    public ListRef? CustomerRef { get; set; }
+    public ListRef? TemplateRef { get; set; }
+    public DateOnly? TxnDate { get; set; }
+    public string? RefNumber { get; set; }
+    public string? PONumber { get; set; }
+    public List<SalesOrderLine>? SalesOrderLines { get; set; }
+
+    internal XElement ToAddRq()
+    {
+        var elem = new XElement("SalesOrderAdd");
+        elem.Append(CustomerRef);
+        elem.Append(TemplateRef);
+        elem.Append(TxnDate);
+        elem.Append(RefNumber);
+        elem.Append(PONumber);
+        foreach (var line in SalesOrderLines ?? [])
+        {
+            elem.Add(line.ToAddRq());
+        }
+        return new XElement("SalesOrderAddRq", elem);
+    }
+}
+
+internal class CreateSalesOrderHandler : IRequestHandler<CreateSalesOrder, Result<SalesOrder>>
+{
+    private readonly QBConnection _conn;
+
+    private readonly ILogger<CreateSalesOrderHandler> _logger;
+
+    public CreateSalesOrderHandler(QBConnection conn, ILogger<CreateSalesOrderHandler> logger)
+    {
+        _conn = conn;
+        _logger = logger;
+    }
+
+    public async Task<Result<SalesOrder>> Handle(CreateSalesOrder request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var qbxml = request.ToAddRq().CreateQBXML().ToXML();
+
+            var result = await _conn.ProcessRequest(qbxml, cancellationToken);
+
+            return SalesOrder.Create(result);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unexpected error handling request: {request}", request);
+
+            return Error.ServerError();
+        }
+
+
+        //return Task.FromResult(Result.Ok(request.ToAddRq()));
+    }
+}
---
+++ QBSync.API/QBSDK/SalesOrders/SalesOrderLine.cs
@@ -0,0 +1,72 @@
+﻿using System.Text.Json.Serialization;
+using System.Xml.Linq;
+
+namespace QBSync.API.QBSDK;
+
+[JsonDerivedType(typeof(SalesOrderItemLine), typeDiscriminator: "SalesOrderItemLine")]
+[JsonDerivedType(typeof(SalesOrderGroupItemLine), typeDiscriminator: "SalesOrderGroupItemLine")]
+public abstract class SalesOrderLine
+{
+    public string TxnLineID { get; set; } = "-1";
+    public ListRef? ItemRef { get; set; }
+    public string? Desc { get; set; }
+    public decimal? Quantity { get; set; }
+
+    public abstract XElement ToAddRq();
+    public abstract XElement ToModRq();
+}
+
+public class SalesOrderGroupItemLine : SalesOrderLine
+{
+    public List<SalesOrderItemLine> Items { get; } = new List<SalesOrderItemLine>();
+
+    public override XElement ToAddRq()
+    {
+        var elem = new XElement("SalesOrderLineGroupAdd");
+        elem.Append(ItemRef, "ItemGroupRef");
+        elem.Append(Quantity);
+        return elem;
+    }
+
+    public override XElement ToModRq()
+    {
+        var elem = new XElement("SalesOrderLineGroupMod");
+        elem.Append(TxnLineID);
+        elem.Append(ItemRef, "ItemGroupRef");
+        elem.Append(Quantity);
+        foreach (var item in Items ?? [])
+        {
+            elem.Add(item.ToModRq());
+        }
+        return elem;
+    }
+}
+
+public class SalesOrderItemLine : SalesOrderLine
+{
+    public decimal? Rate { get; set; }
+    public decimal? Amount { get; set; }
+
+    public override XElement ToAddRq()
+    {
+        var elem = new XElement("SalesOrderLineAdd");
+        elem.Append(ItemRef);
+        elem.Append(Desc);
+        elem.Append(Quantity);
+        elem.Append(Rate);
+        elem.Append(Amount);
+        return elem;
+    }
+
+    public override XElement ToModRq()
+    {
+        var elem = new XElement("SalesOrderLineMod");
+        elem.Append(TxnLineID);
+        elem.Append(ItemRef);
+        elem.Append(Desc);
+        elem.Append(Quantity);
+        elem.Append(Rate);
+        elem.Append(Amount);
+        return elem;
+    }
+}
