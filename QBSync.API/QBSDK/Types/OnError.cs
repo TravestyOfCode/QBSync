20c657870b0c4a4c3467d21642cd26844ce18ad0
travis@travestyofcode.com <travis@travestyofcode.com>
2025-03-29 07:12:14 -0400

    Adds OnError type for generating QBXML message requests.

---
+++ QBSync.API/QBSDK/Types/OnError.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QBSync.API.QBSDK;
+
+public enum OnError
+{
+    [Display(Name = "Stop on Error")]
+    stopOnError,
+
+    [Display(Name = "Continue on Error")]
+    continueOnError
+}
