9b0b9ded1f78d71a1fdf57dca055c7f2d3ef554e
travis@travestyofcode.com <travis@travestyofcode.com>
2025-03-29 07:42:35 -0400

    Adds SalesOrderController and updates http file for testing API calls.

---
+++ QBSync.API/Controllers/SalesOrderController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using QBSync.API.QBSDK;
+
+namespace QBSync.API.Controllers;
+
+[ApiController()]
+[Route("{controller}/{action}")]
+public class SalesOrderController : Controller
+{
+    private readonly IMediator _mediator;
+
+    public SalesOrderController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateSalesOrder request, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(request, cancellationToken);
+
+        return StatusCode(result.StatusCode, result.Value?.ToString());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Update([FromBody] UpdateSalesOrder request, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(request, cancellationToken);
+
+        return StatusCode(result.StatusCode, result.Value?.ToString());
+    }
+}
