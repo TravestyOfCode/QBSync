2b836738719b27c61eb7cff60b27a0ecf79cea54
travis@travestyofcode.com <travis@travestyofcode.com>
2025-03-29 07:09:23 -0400

    Adds functions to create a QBXML request string from a single request element.

--- QBSync.API/QBSDK/XElementExtensions.cs
+++ QBSync.API/QBSDK/XElementExtensions.cs
@@ -1,10 +1,34 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
 using System.Xml.Linq;
 
 namespace QBSync.API.QBSDK;
 
 internal static class XElementExtensions
 {
+    public static XDocument CreateQBXML(this XElement request, OnError onError = OnError.stopOnError)
+    {
+        var msgsRq = new XElement("QBXMLMsgsRq", request);
+        msgsRq.Add(new XAttribute(nameof(onError), onError));
+
+        var result = new XDocument(new XProcessingInstruction("qbxml", "version=\"13.0\""));
+        result.Declaration = new XDeclaration("1.0", "utf-8", null);
+
+        result.Add(new XElement("QBXML", msgsRq));
+        return result;
+    }
+
+    public static string ToXML(this XDocument document)
+    {
+        var builder = new StringBuilder();
+        using (TextWriter writer = new StringWriter(builder))
+        {
+            document.Save(writer);
+            return builder.ToString();
+        }
+    }
+
     public static void Append(this XElement element, string? value, [CallerArgumentExpression(nameof(value))] string name = "")
     {
         if (value != null)
